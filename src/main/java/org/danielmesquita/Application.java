20dd17ca189165ad9403f34b5f659ed08fec9e53
Daniel Mesquita <danielmesquitadev@gmail.com>
2024-09-04 21:07:53 -0300

    feat: add database change methods

--- src/main/java/org/danielmesquita/Application.java
+++ src/main/java/org/danielmesquita/Application.java
@@ -1,16 +1,12 @@
 package org.danielmesquita;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.danielmesquita.constants.QueriesSQL;
 import org.danielmesquita.dbconfig.DB;
-import org.danielmesquita.entities.Order;
-import org.danielmesquita.entities.OrderStatus;
+import org.danielmesquita.dbconfig.DbException;
 import org.danielmesquita.entities.Product;
 
 public class Application {
@@ -19,42 +15,51 @@ public class Application {
 
     Statement statement = connection.createStatement();
 
-    ResultSet resultSet = statement.executeQuery(QueriesSQL.INNER_JOIN_TABLES);
-
-    Map<Long, Order> orderMap = new HashMap<>();
+    ResultSet resultSet = statement.executeQuery(QueriesSQL.FIND_ALL_PRODUCTS);
 
     Map<Long, Product> productMap = new HashMap<>();
 
     while (resultSet.next()) {
-      Long orderId = resultSet.getLong("order_id");
-      if (orderMap.get(orderId) == null) {
-        Order order = instantiateOrder(resultSet);
-        orderMap.put(orderId, order);
-      }
-
       Long productId = resultSet.getLong("product_id");
       if (productMap.get(productId) == null) {
         Product product = instantiateProduct(resultSet);
         productMap.put(productId, product);
       }
 
-      orderMap.get(orderId).getProducts().add(productMap.get(productId));
+      System.out.println("Product: " + productMap.get(productId));
     }
 
-    for (Long orderId : orderMap.keySet()) {
-      System.out.println(orderMap.get(orderId));
-      for (Product product : orderMap.get(orderId).getProducts()) {
-        System.out.println(product);
-      }
-      System.out.println();
-    }
+    Product productToUpdate = new Product();
+    productToUpdate.setId(1L);  // Supondo que você quer atualizar o produto com ID 1
+    productToUpdate.setName("Samsung Galaxy S21");
+    productToUpdate.setPrice(2000.0);
+    productToUpdate.setDescription("The best Android phone ever");
+    productToUpdate.setImageUri("https://www.samsung.com/samsung-galaxy-s21.jpg");
+
+    updateProduct(productToUpdate);
+
+    System.out.println("Product updated: " + productToUpdate);
+
+    Product newProduct = new Product();
+    newProduct.setName("Iphone 13");
+    newProduct.setPrice(3000.0);
+    newProduct.setDescription("The best iPhone ever");
+    newProduct.setImageUri("https://apple.com/iphone/4.png");
+
+    insertProduct(newProduct);
+
+    System.out.println("New product inserted: " + newProduct);
+
+    deleteProductById(newProduct.getId());
+
+    System.out.println("Product deleted: " + newProduct.getId());
   }
 
   private static Product instantiateProduct(ResultSet resultSet) throws SQLException {
     Product product = new Product();
     product.setId(
         resultSet.getLong(
-            "product_id")); // product_id instead of id to avoid conflict with Order id
+            "product_id"));
     product.setName(resultSet.getString("name"));
     product.setPrice(resultSet.getDouble("price"));
     product.setDescription(resultSet.getString("description"));
@@ -62,14 +67,60 @@ public class Application {
     return product;
   }
 
-  private static Order instantiateOrder(ResultSet resultSet) throws SQLException {
-    Order order = new Order();
-    order.setId(
-        resultSet.getLong("order_id")); // order_id instead of id to avoid conflict with Product id
-    order.setLatitude(resultSet.getDouble("latitude"));
-    order.setLongitude(resultSet.getDouble("longitude"));
-    order.setMoment(resultSet.getTimestamp("moment").toInstant());
-    order.setStatus(OrderStatus.values()[resultSet.getInt("status")]);
-    return order;
+  public static void insertProduct(Product product) {
+    try (Connection connection = DB.getConnection();
+        PreparedStatement preparedStatement =
+            connection.prepareStatement(QueriesSQL.INSERT_PRODUCTS, Statement.RETURN_GENERATED_KEYS)) {
+
+      preparedStatement.setString(1, product.getName());
+      preparedStatement.setDouble(2, product.getPrice());
+      preparedStatement.setString(3, product.getDescription());
+      preparedStatement.setString(4, product.getImageUri());
+
+      int rowsAffected = preparedStatement.executeUpdate();
+
+      if (rowsAffected > 0) {
+        try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+          if (resultSet.next()) {
+            product.setId(resultSet.getLong(1));
+          }
+        }
+      }
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    }
+  }
+
+  public static void deleteProductById(long id) {
+    try (Connection connection = DB.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(QueriesSQL.DELETE_PRODUCT)) {
+
+      preparedStatement.setLong(1, id);
+
+      preparedStatement.executeUpdate();
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    }
+  }
+
+  public static void updateProduct(Product product) {
+    try (Connection connection = DB.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(QueriesSQL.UPDATE_PRODUCT)) {
+
+      preparedStatement.setString(1, product.getName());
+      preparedStatement.setDouble(2, product.getPrice());
+      preparedStatement.setString(3, product.getDescription());
+      preparedStatement.setString(4, product.getImageUri());
+      preparedStatement.setLong(5, product.getId());
+
+      int rowsAffected = preparedStatement.executeUpdate();
+
+      if (rowsAffected == 0) {
+        throw new DbException("No product found with the given ID: " + product.getId());
+      }
+
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    }
   }
 }
--- src/main/java/org/danielmesquita/constants/QueriesSQL.java
+++ src/main/java/org/danielmesquita/constants/QueriesSQL.java
@@ -7,4 +7,18 @@ public class QueriesSQL {
       "SELECT * FROM tb_order "
           + " INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id "
           + " INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id";
+  public static final String INSERT_PRODUCTS =
+      "INSERT INTO tb_product (name, price, image_uri, description) VALUES (?, ?, ?, ?)";
+  public static final String INSERT_ORDER =
+      "INSERT INTO tb_order (latitude, longitude, moment, status) VALUES (?, ?, ?, ?)";
+  public static final String INSERT_ORDER_PRODUCT =
+      "INSERT INTO tb_order_product (order_id, product_id) VALUES (?, ?)";
+  public static final String DELETE_ORDER_PRODUCT =
+      "DELETE FROM tb_order_product WHERE order_id = ?";
+  public static final String DELETE_ORDER = "DELETE FROM tb_order WHERE id = ?";
+  public static final String DELETE_PRODUCT = "DELETE FROM tb_product WHERE id = ?";
+  public static final String UPDATE_PRODUCT =
+      "UPDATE tb_product SET name = ?, price = ?, image_uri = ?, description = ? WHERE id = ?";
+    public static final String UPDATE_ORDER =
+      "UPDATE tb_order SET latitude = ?, longitude = ?, moment = ?, status = ? WHERE id = ?";
 }
